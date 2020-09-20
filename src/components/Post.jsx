7ab62b1a873408120b58839f8ac53ab2e27de063
Satvir Sandhu <356Sandhu@gmail.com>
2020-09-20 15:41:33 -0400

    implement updating with stars

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,6 +6,7 @@ import moment from "moment";
 const Post = ({ title, content, user, createdAt, stars, comments, id }) => {
   const postRef = firestore.doc(`posts/${id}`);
   const remove = () => postRef.delete();
+  const star = () => postRef.update({ stars: stars + 1 });
 
   return (
     <article className="Post">
@@ -28,10 +29,12 @@ const Post = ({ title, content, user, createdAt, stars, comments, id }) => {
             {comments}
           </p>
           <p>Posted by {user.displayName}</p>
-          <p>{moment(createdAt).calendar()}</p>
+          <p>{moment(createdAt.toDate()).calendar()}</p>
         </div>
         <div>
-          <button className="star">Star</button>
+          <button className="star" onClick={star}>
+            Star
+          </button>
           <button className="delete" onClick={remove}>
             Delete
           </button>
