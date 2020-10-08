e8b402c86a9e15423350ee6b7d01180b4bab5ff7
Satvir Sandhu <356Sandhu@gmail.com>
2020-10-08 14:19:06 -0400

    finish comments

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -4,13 +4,59 @@ import Post from "./Post";
 import Comments from "./Comments";
 import { firestore } from "../firebase";
 import { collectIdsAndDocs } from "../utilities";
+import { withRouter } from "react-router-dom";
+import withUser from "./withUser";
 
 class PostPage extends Component {
   state = { post: null, comments: [] };
 
+  get postId() {
+    return this.props.match.params.id;
+  }
+
+  get postRef() {
+    return firestore.doc(`posts/${this.postId}`);
+  }
+
+  get commentsRef() {
+    return this.postRef.collection("comments");
+  }
+
+  unsubscribeFromPost = null;
+  unsubscribeFromComments = null;
+
+  componentDidMount = async () => {
+    this.unsubscribeFromPost = this.postRef.onSnapshot((snapshot) => {
+      const post = collectIdsAndDocs(snapshot);
+      this.setState({ post });
+    });
+    this.unsubscribeFromComments = this.commentsRef.onSnapshot((snapshot) => {
+      const comments = snapshot.docs.map(collectIdsAndDocs);
+      this.setState({ comments });
+    });
+  };
+
+  componentWillUnmount = () => {
+    this.unsubscribeFromPost();
+    this.unsubscribeFromComments();
+  };
+
+  createComment = (comment) => {
+    this.commentsRef.add({
+      ...comment,
+    });
+  };
+
   render() {
-    return <div>Post Page!</div>;
+    const { post, comments } = this.state;
+    console.log(this.props);
+    return (
+      <section>
+        {post && <Post {...post} />}
+        <Comments comments={comments} onCreate={this.createComment} />
+      </section>
+    );
   }
 }
 
-export default PostPage;
+export default withRouter(withUser(PostPage));
